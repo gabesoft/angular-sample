5ef1fce6a4efa7f4c163104510890bc7bf7ccf1b
Gabriel Adomnicai <gabesoft@gmail.com>
2014-10-12 13:42:54 -0700

    angular config + service

--- client/app.js
+++ client/app.js
@@ -1,24 +1,40 @@
 (function () {
     var app = angular.module('app', []);
 
+    app.config(function ($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    });
+
     app.filter('userUrl', function () {
         return function (username) {
             return 'www.creativelive.com/users/' + username;
         };
     });
 
-    app.controller('VerifyController', [ '$scope', function ($scope) {
-        this.verify = function () {
-            $scope.checking = true;
+    app.service('UserService', function () {
+        this.verify = function (password, cb) {
             setTimeout(function () {
-                $scope.verified = ($scope.password === 'abc');
-                $scope.checking = false;
-                $scope.$apply();
+                cb(null, password === 'abcd');
             }, 1000);
+        };
+    });
+
+    app.controller('VerifyController', [ '$scope', '$timeout', 'UserService', function ($scope, $timeout, UserService) {
+        this.verify = function () {
+            $timeout.cancel(this.verifyFn);
+            this.verifyFn = $timeout(function () {
+                $scope.checking = true;
+                UserService.verify($scope.password, function (err, verified) {
+                    console.log('VERIFIED', verified);
+                    $scope.verified = verified;
+                    $scope.checking = false;
+                    $scope.$apply();
+                });
+            }, 300);
         };
     }]);
 
-    app.controller('SettingsController', function () {
+    app.controller('SettingsController', [ '$scope', function ($scope) {
         this.user = { timezone: 'AL' };
 
         this.initialize = function () {
@@ -34,6 +50,7 @@
         };
 
         this.update = function () {
+            $scope.settingsForm.$setPristine();
             console.log('updating user...');
         };
 
@@ -42,7 +59,7 @@
             delete copy.password;
             return angular.equals({ timezone: 'AL' }, copy);
         };
-    });
+    }]);
 
     angular.element(document).ready(function () {
         setTimeout(function () {
